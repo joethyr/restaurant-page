7817c767f9e10d4c1b7f67a791f05839e2f27cd0
joethyr <joethayr@gmail.com>
2023-01-01 14:07:35 -0700

    refactor javascript

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
+
 const content = document.getElementById('content')
 const home = document.createElement("div");
 home.classList.add('home')
 content.appendChild(home)
 
-const headerOne = document.createElement('h1')
-headerOne.innerHTML = "JOE'S PIZZERIA";
-home.appendChild(headerOne)
+function addDomElement(element, text) {
+  let i = document.createElement(element)
+  i.innerHTML = text;
+  home.appendChild(i)
+}
 
-const headerTwo = document.createElement('h2')
-headerTwo.innerHTML = "ITS THE ONLY PIZZERIA YOU CAN TRUST!<br>FREE TOPPINGS!";
-home.appendChild(headerTwo)
-
-const homeButton = document.createElement('button')
-homeButton.innerHTML = "Order Now";
-home.appendChild(homeButton)
-
-const homeDescription = document.createElement('p')
-homeDescription.classList.add('home-description')
-homeDescription.innerHTML = "Let me tell you just how great Joe's Pizzeria is in great lorem ipsum detail. Lorem ipsum, porro sunt riendis iusto quibusdam quisquam assumenda qui provident ducimus quos maiores inventore tempora corporis aut animi fugit, quis atque?"
-home.appendChild(homeDescription)
-
+addDomElement("h1", "JOE'S PIZZERIA")
+addDomElement("h2", "ITS THE ONLY PIZZERIA YOU CAN TRUST!<br>FREE TOPPINGS!")
+addDomElement("button", "ORDER NOW")
+addDomElement("p", "Let me tell you just how great Joe's Pizzeria is in great lorem ipsum detail. Lorem ipsum, porro sunt riendis iusto quibusdam quisquam assumenda qui provident ducimus quos maiores inventore tempora corporis aut animi fugit, quis atque?", "home-description")
