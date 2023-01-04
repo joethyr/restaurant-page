377b350a0ba98763a076d1a02e6a474e912e719d
joethyr <joethayr@gmail.com>
2023-01-03 19:46:22 -0700

    update css and html with navbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-
 const content = document.getElementById('content')
 const home = document.createElement("div");
+const navbar = document.createElement("ul");
+
 home.classList.add('home')
 content.appendChild(home)
+
 
 function addDomElement(element, text) {
   let i = document.createElement(element)
@@ -10,7 +12,6 @@ function addDomElement(element, text) {
   home.appendChild(i)
 }
 
-addDomElement("h1", "JOE'S PIZZERIA")
 addDomElement("h2", "ITS THE ONLY PIZZERIA YOU CAN TRUST!<br>FREE TOPPINGS!")
 addDomElement("button", "ORDER NOW")
 addDomElement("p", "Let me tell you just how great Joe's Pizzeria is in great lorem ipsum detail. Lorem ipsum, porro sunt riendis iusto quibusdam quisquam assumenda qui provident ducimus quos maiores inventore tempora corporis aut animi fugit, quis atque?", "home-description")
